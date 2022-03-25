32b5b164c855cda0544ffd517d5902bf1aaf6011
elisarissanen <elisa.rissanen1@gmail.com>
2022-03-26 00:18:36 +0200

    Initial project files and project idea

---
+++ initial_file.py
@@ -0,0 +1,33 @@
+#####################
+#                   #
+#   TASK MANAGER    #
+#       v.1         #
+#                   #
+#####################
+
+# Inital setup when opening, import from file
+
+
+# Read the file - in a nice way
+# Check not started / in progress / done tasks
+
+# Add a task to the file
+# Default values if string empty?
+
+# Make task in progress
+# Make task in done - add TIME SPENT
+
+# SETTINGS
+# Check task categories
+# Chek subclass categories
+
+# Task includes
+# TASK NAME
+# TASK CATEGORY - Home/School/Work/Personal
+# TASK SUBCLASS - Based on class
+# TIME ESTIMATE
+# TIME SPENT
+# STATUS - not started, in progress, done
+# IMPORTANCE 1-5 (1 is most important)
+
+# Import task list based on importance and time estimated
